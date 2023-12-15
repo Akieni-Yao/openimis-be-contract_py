8632b694cd07db7a107137a16bc034f9b9808442
LAKSHYA SONI <lakshya.soni@walkingtree.tech>
2023-12-15 14:02:08 +0530

    removed mapping for employer declaration certificate

--- contract/email_report.py
+++ contract/email_report.py
@@ -8,29 +8,29 @@ from report.services import get_report_definition, generate_report
 
 def generate_report_for_employee_declaration(code, uuid, contract_approved_date, amount_due):
     try:
-        policyholder = PolicyHolder.objects.get(uuid=uuid)
-        json_ext_data = policyholder.json_ext['jsonExt'] if policyholder.json_ext else {}
-        address_data = policyholder.address['address'] if policyholder.address else {}
-        activity_code = json_ext_data.get('activityCode', '')  # Use .get() to avoid KeyError
-        converted_activity_code = convert_activity_data(activity_code)
-        converted_creation_date = str(contract_approved_date.strftime('%d-%m-%Y'))
-
+        # policyholder = PolicyHolder.objects.get(uuid=uuid)
+        # json_ext_data = policyholder.json_ext['jsonExt'] if policyholder.json_ext else {}
+        # address_data = policyholder.address['address'] if policyholder.address else {}
+        # activity_code = json_ext_data.get('activityCode', '')  # Use .get() to avoid KeyError
+        # converted_activity_code = convert_activity_data(activity_code)
+        # converted_creation_date = str(contract_approved_date.strftime('%d-%m-%Y'))
+        # ad = str(amount_due)
         data = {
             "data": {
                 "rib": "",  # TBD
-                "contract_number": code if code else "",
-                "creation_date": converted_creation_date if converted_creation_date else "",
-                "camu_code": policyholder.code if policyholder.code else "",
-                "activity_code": converted_activity_code if converted_activity_code else "",
-                "niu": policyholder.json_ext['jsonExt']['niu'] if policyholder.json_ext else "",
-                "address": policyholder.address['address'] if policyholder.address['address'] else "",
-                "phone": policyholder.phone if policyholder.phone else "",
-                "totalinsurees": policyholder.json_ext['jsonExt']['nbEmployees'] if policyholder.json_ext else "",
+                "contract_number": "",
+                "creation_date":  "",
+                "camu_code":  "",
+                "activity_code": "",
+                "niu":  "",
+                "address":  "",
+                "phone":"",
+                "totalinsurees":  "",
                 "contribution_sum": "",  # TBD
                 "subscriber_contribution": "",  # TBD
                 "name": "",  # TBD
                 "insured_contribution": "",  # TBD
-                "expected_amout": str(amount_due),
+                "expected_amout": "",
             }
         }
         report_config = ReportConfig.get_report('certificate_declaration_insured')
@@ -42,8 +42,6 @@ def generate_report_for_employee_declaration(code, uuid, contract_approved_date,
         template_dict = json.loads(report_definition)
         pdf = generate_report('certificate_declaration_insured', template_dict, data)
         return pdf
-    except PolicyHolder.DoesNotExist:
-        raise Http404("Policy holder not found")
     except Exception as e:
         print(f"An error occurred: {e}")
         return None
