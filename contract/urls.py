a6b84f4754435df7a2252cc5b07266039a29cab0
LAKSHYA SONI <lakshya.soni@walkingtree.tech>
2024-08-28 13:36:55 +0530

    CS - 1481 - BE - New API creation to upload excel sheet and manage response status

--- contract/urls.py
+++ contract/urls.py
@@ -4,4 +4,5 @@ from contract import views
 
 urlpatterns = [
     path("export/<contract_id>/contracts", views.multi_contract),
+    path("import/<contract_id>/contracts/salaries", views.update_contract_salaries),
 ]
