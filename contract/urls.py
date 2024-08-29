2591d72b16d5d3f72a881abf2a05be0f2d57c069
akanksha-paliwal <akanksha.paliwal@walkingtree.tech>
2024-08-29 11:45:58 +0530

    API for payment method line

--- contract/urls.py
+++ contract/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from contract import views
+from contract import views, erp_integrations
 
 urlpatterns = [
     path("export/<contract_id>/contracts", views.multi_contract),
+    path('payment-methods/<int:journal_id>', erp_integrations.erp_payment_method_line),
+
 ]
