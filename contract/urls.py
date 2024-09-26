ca6c6b1ad94b78d66c901d0b8f2f036839f005f7
LAKSHYA SONI <lakshya.soni@walkingtree.tech>
2024-09-26 12:02:14 +0530

    Update urls.py

--- contract/urls.py
+++ contract/urls.py
@@ -4,5 +4,5 @@ from contract import views, erp_integrations
 
 urlpatterns = [
     path("export/<contract_id>/contracts", views.multi_contract),
-
+    path("import/<contract_id>/contracts/salaries", views.update_contract_salaries),
 ]
