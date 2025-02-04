c1bd9e4db0ecd8f7ef331972d961d6e777020289
tom hikari <tomprescott1565@gmail.com>
2025-02-04 15:35:06 +0100

    feature report file

--- contract/schema.py
+++ contract/schema.py
@@ -12,25 +12,48 @@ from django.db.models import Q
 from policyholder.models import PolicyHolder, PolicyHolderContributionPlan
 from .services import check_unique_code
 from core.gql_queries import ValidationMessageGQLType
-from core.schema import signal_mutation_module_before_mutating, OrderedDjangoFilterConnectionField
+from core.schema import (
+    signal_mutation_module_before_mutating,
+    OrderedDjangoFilterConnectionField,
+)
 from core.utils import append_validity_filter
-from contract.models import Contract, ContractDetails, \
-    ContractContributionPlanDetails, ContractMutation
-from contract.gql.gql_types import ContractGQLType, ContractDetailsGQLType, \
-    ContractContributionPlanDetailsGQLType
+from contract.models import (
+    Contract,
+    ContractDetails,
+    ContractContributionPlanDetails,
+    ContractMutation,
+)
+from contract.gql.gql_types import (
+    ContractGQLType,
+    ContractDetailsGQLType,
+    ContractContributionPlanDetailsGQLType,
+)
 
-from contract.gql.gql_mutations.contract_mutations import CreateContractMutation, \
-    UpdateContractMutation, DeleteContractMutation, SubmitContractMutation, ApproveContractMutation, \
-    ApproveContractBulkMutation, CounterContractMutation, \
-    AmendContractMutation, RenewContractMutation, CounterContractBulkMutation, ContractCreateInvoiceBulkMutation
-from contract.gql.gql_mutations.contract_details_mutations import CreateContractDetailsMutation, \
-    UpdateContractDetailsMutation, DeleteContractDetailsMutation, \
-    CreateContractDetailByPolicyHolderInsureeMutation
+from contract.gql.gql_mutations.contract_mutations import (
+    CreateContractMutation,
+    UpdateContractMutation,
+    DeleteContractMutation,
+    SubmitContractMutation,
+    ApproveContractMutation,
+    ApproveContractBulkMutation,
+    CounterContractMutation,
+    AmendContractMutation,
+    RenewContractMutation,
+    CounterContractBulkMutation,
+    ContractCreateInvoiceBulkMutation,
+    PrintContractReceiptMutation,
+)
+from contract.gql.gql_mutations.contract_details_mutations import (
+    CreateContractDetailsMutation,
+    UpdateContractDetailsMutation,
+    DeleteContractDetailsMutation,
+    CreateContractDetailByPolicyHolderInsureeMutation,
+)
 from contract.apps import ContractConfig
 from contract.utils import filter_amount_contract
 
 logger = logging.getLogger(__name__)
-erp_url = os.environ.get('ERP_HOST', "https://camu-staging-15480786.dev.odoo.com")
+erp_url = os.environ.get("ERP_HOST", "https://camu-staging-15480786.dev.odoo.com")
 
 
 class Query(graphene.ObjectType):
@@ -43,7 +66,7 @@ class Query(graphene.ObjectType):
         dateValidTo__Lte=graphene.DateTime(),
         amount_from=graphene.Decimal(),
         amount_to=graphene.Decimal(),
-        applyDefaultValidityFilter=graphene.Boolean()
+        applyDefaultValidityFilter=graphene.Boolean(),
     )
 
     contract_details = OrderedDjangoFilterConnectionField(
@@ -62,19 +85,21 @@ class Query(graphene.ObjectType):
     validate_contract_code = graphene.Field(
         ValidationMessageGQLType,
         contract_code=graphene.String(required=True),
-        description="Check that the specified contract code is unique."
+        description="Check that the specified contract code is unique.",
     )
 
     validate_enddate_by_periodicity = graphene.String(
         start_date=graphene.Date(required=False),
+        policyholder_id=graphene.UUID(required=True),
+    )
+
+    validate_startdate_based_on_last_contract = graphene.String(
         policyholder_id=graphene.UUID(required=True)
     )
 
-    validate_startdate_based_on_last_contract= graphene.String(
-        policyholder_id=graphene.UUID(required=True)
-    )
-
-    def resolve_validate_enddate_by_periodicity(self, info, start_date=None, policyholder_id=None):
+    def resolve_validate_enddate_by_periodicity(
+        self, info, start_date=None, policyholder_id=None
+    ):
         try:
             policy_holder = PolicyHolder.objects.filter(id=policyholder_id).first()
             if not policy_holder:
@@ -84,9 +109,13 @@ class Query(graphene.ObjectType):
                 policy_holder=policy_holder, is_deleted=False
             ).first()
 
-            contract = Contract.objects.filter(
-                policy_holder__id=policyholder_id, is_deleted=False
-            ).order_by('-date_valid_to').first()
+            contract = (
+                Contract.objects.filter(
+                    policy_holder__id=policyholder_id, is_deleted=False
+                )
+                .order_by("-date_valid_to")
+                .first()
+            )
 
             if ph_cpb:
                 contribution_plan_bundle = ph_cpb.contribution_plan_bundle
@@ -100,41 +129,55 @@ class Query(graphene.ObjectType):
                     else:
                         start_date = next_day
                 elif start_date is None:
-                    return {"error": "No start date provided and no previous contract found"}
+                    return {
+                        "error": "No start date provided and no previous contract found"
+                    }
 
                 if start_date and periodicity is not None:
                     if 1 <= periodicity <= 3:
                         if periodicity == 1:
-                            _, last_day_of_month = monthrange(start_date.year, start_date.month)
-                            end_date = start_date + timedelta(days=(periodicity * last_day_of_month) - 1)
+                            _, last_day_of_month = monthrange(
+                                start_date.year, start_date.month
+                            )
+                            end_date = start_date + timedelta(
+                                days=(periodicity * last_day_of_month) - 1
+                            )
                         else:
                             end_date = start_date + relativedelta(months=periodicity)
                             end_date -= timedelta(days=1)
                     elif periodicity == 12:
                         is_exist = Contract.objects.filter(
-                            policy_holder__id=policyholder_id, is_deleted=False,
-                            date_valid_from__gte=start_date
+                            policy_holder__id=policyholder_id,
+                            is_deleted=False,
+                            date_valid_from__gte=start_date,
                         )
                         if not is_exist and not contract:
                             end_date = start_date + relativedelta(months=periodicity)
                             end_date -= timedelta(days=1)
-                        elif not is_exist and start_date >= (contract.date_valid_from + timedelta(days=1)).date():
+                        elif (
+                            not is_exist
+                            and start_date
+                            >= (contract.date_valid_from + timedelta(days=1)).date()
+                        ):
                             end_date = start_date + relativedelta(months=periodicity)
                             end_date -= timedelta(days=1)
                         else:
-                            return {"error": "Invalid Month! Contract of Current or Previous Month is already created."}
+                            return {
+                                "error": "Invalid Month! Contract of Current or Previous Month is already created."
+                            }
                     else:
                         return {"error": f"Invalid periodicity value: {periodicity}"}
                 else:
-                    return {"error": "Periodicity is not defined for this Contribution Plan Bundle"}
+                    return {
+                        "error": "Periodicity is not defined for this Contribution Plan Bundle"
+                    }
 
                 # Return both start_date and end_date
-                return {
-                    "start_date": str(start_date),
-                    "end_date": str(end_date)
-                }
+                return {"start_date": str(start_date), "end_date": str(end_date)}
             else:
-                return {"error": "Contribution Plan Bundle not found for this Policy Holder"}
+                return {
+                    "error": "Contribution Plan Bundle not found for this Policy Holder"
+                }
 
         except Exception as e:
             print(f"Error: {e}")
@@ -142,9 +185,11 @@ class Query(graphene.ObjectType):
 
     def resolve_validate_contract_code(self, info, **kwargs):
         if not info.context.user.has_perms(ContractConfig.gql_query_contract_perms):
-            if not info.context.user.has_perms(ContractConfig.gql_query_contract_policyholder_portal_perms):
+            if not info.context.user.has_perms(
+                ContractConfig.gql_query_contract_policyholder_portal_perms
+            ):
                 raise PermissionError("Unauthorized")
-        errors = check_unique_code(code=kwargs['contract_code'])
+        errors = check_unique_code(code=kwargs["contract_code"])
         if errors:
             return ValidationMessageGQLType(False)
         else:
@@ -152,51 +197,59 @@ class Query(graphene.ObjectType):
 
     def resolve_contract(self, info, **kwargs):
         if not info.context.user.has_perms(ContractConfig.gql_query_contract_perms):
-            if not info.context.user.has_perms(ContractConfig.gql_query_contract_policyholder_portal_perms):
+            if not info.context.user.has_perms(
+                ContractConfig.gql_query_contract_policyholder_portal_perms
+            ):
                 raise PermissionError("Unauthorized")
 
         filters = append_validity_filter(**kwargs)
         client_mutation_id = kwargs.get("client_mutation_id", None)
         if client_mutation_id:
-            filters.append(Q(mutations__mutation__client_mutation_id=client_mutation_id))
+            filters.append(
+                Q(mutations__mutation__client_mutation_id=client_mutation_id)
+            )
 
-        insuree = kwargs.get('insuree', None)
+        insuree = kwargs.get("insuree", None)
         if insuree:
             filters.append(Q(contractdetails__insuree__uuid=insuree))
 
         # amount filters
-        amount_from = kwargs.get('amount_from', None)
-        amount_to = kwargs.get('amount_to', None)
+        amount_from = kwargs.get("amount_from", None)
+        amount_to = kwargs.get("amount_to", None)
         if amount_from or amount_to:
             filters.append(filter_amount_contract(**kwargs))
         return gql_optimizer.query(Contract.objects.filter(*filters).all(), info)
 
     def resolve_contract_details(self, info, **kwargs):
         if not info.context.user.has_perms(ContractConfig.gql_query_contract_perms):
-            if not info.context.user.has_perms(ContractConfig.gql_query_contract_policyholder_portal_perms):
+            if not info.context.user.has_perms(
+                ContractConfig.gql_query_contract_policyholder_portal_perms
+            ):
                 raise PermissionError("Unauthorized")
 
         filters = []
         client_mutation_id = kwargs.get("client_mutation_id", None)
         if client_mutation_id:
-            filters.append(Q(mutations__mutation__client_mutation_id=client_mutation_id))
+            filters.append(
+                Q(mutations__mutation__client_mutation_id=client_mutation_id)
+            )
 
         return gql_optimizer.query(ContractDetails.objects.filter(*filters).all(), info)
 
     def resolve_contract_contribution_plan_details(self, info, **kwargs):
         if not info.context.user.has_perms(ContractConfig.gql_query_contract_perms):
-            if not info.context.user.has_perms(ContractConfig.gql_query_contract_policyholder_portal_perms):
+            if not info.context.user.has_perms(
+                ContractConfig.gql_query_contract_policyholder_portal_perms
+            ):
                 raise PermissionError("Unauthorized")
 
         query = ContractContributionPlanDetails.objects.all()
 
-        insuree = kwargs.get('insuree', None)
-        contribution_plan_bundle = kwargs.get('contributionPlanBundle', None)
+        insuree = kwargs.get("insuree", None)
+        contribution_plan_bundle = kwargs.get("contributionPlanBundle", None)
 
         if insuree:
-            query = query.filter(
-                contract_details__insuree__uuid=insuree
-            )
+            query = query.filter(contract_details__insuree__uuid=insuree)
 
         if contribution_plan_bundle:
             query = query.filter(
@@ -218,23 +271,28 @@ class Mutation(graphene.ObjectType):
     amend_contract = AmendContractMutation.Field()
     renew_contract = RenewContractMutation.Field()
     create_contract_invoice_bulk = ContractCreateInvoiceBulkMutation.Field()
+    print_contract_receipt = PrintContractReceiptMutation.Field()
 
     create_contract_details = CreateContractDetailsMutation.Field()
     update_contract_details = UpdateContractDetailsMutation.Field()
     delete_contract_details = DeleteContractDetailsMutation.Field()
-    create_contract_details_by_ph_insuree = CreateContractDetailByPolicyHolderInsureeMutation.Field()
+    create_contract_details_by_ph_insuree = (
+        CreateContractDetailByPolicyHolderInsureeMutation.Field()
+    )
 
 
 def on_contract_mutation(sender, **kwargs):
-    uuids = kwargs['data'].get('uuids', [])
+    uuids = kwargs["data"].get("uuids", [])
     if not uuids:
-        uuid = kwargs['data'].get('uuid', None)
+        uuid = kwargs["data"].get("uuid", None)
         uuids = [uuid] if uuid else []
     if not uuids:
         return []
     impacted_contracts = Contract.objects.filter(id__in=uuids).all()
     for contract in impacted_contracts:
-        ContractMutation.objects.update_or_create(contract=contract, mutation_id=kwargs['mutation_log_id'])
+        ContractMutation.objects.update_or_create(
+            contract=contract, mutation_id=kwargs["mutation_log_id"]
+        )
     return []
 
 
--- contract/utils.py
+++ contract/utils.py
@@ -1,5 +1,7 @@
 from django.db.models import Q
 
+from contract.models import Contract
+
 
 def filter_amount_contract(arg='amount_from', arg2='amount_to', **kwargs):
     amount_from = kwargs.get(arg)
@@ -32,3 +34,82 @@ def filter_amount_contract(arg='amount_from', arg2='amount_to', **kwargs):
             Q(amount_rectified__gte=amount_from, amount_rectified__lte=amount_to, state__in=status_rectified) |
             Q(amount_due__gte=amount_from, amount_due__lte=amount_to, state__in=status_due)
         )
+
+
+def generate_report_for_contract_receipt(contract_id):
+    from core import datetime
+
+    now = datetime.datetime.now()
+    try:
+        contract = Contract.objects.filter(id=contract_id, is_deleted=False).first()
+        if contract:
+            contract_details
+            # ccpd = ContractContributionPlanDetails.objects.filter(
+            #     contribution_id=pd.premium.id
+            # ).first()
+            # if ccpd and ccpd.contract_details and ccpd.contract_details.contract:
+            # contract = ccpd.contract_details.contract
+            policy_holder = contract.policy_holder
+            # payment = pd.payment
+            received_amount = str(payment.received_amount)
+            received_date = payment.received_date
+            if received_date:
+                formatted_date = str(received_date.strftime("%d-%m-%Y"))
+            else:
+                formatted_date = ""
+            receipt_number = str(payment.receipt_no)
+            report_name = "payment_receipt"
+            activity_code = policy_holder.json_ext["jsonExt"]["activityCode"]
+            converted_activity_code = convert_activity_data(activity_code)
+            converted_creation_date = str(now.strftime("%d-%m-%Y"))
+            data = {
+                "data": {
+                    "camu_code": policy_holder.code if policy_holder else "",
+                    "activity_code": (
+                        policy_holder.trade_name if policy_holder else ""
+                    ),
+                    "reg_date": (
+                        converted_creation_date if converted_creation_date else ""
+                    ),
+                    "niu": (
+                        policy_holder.json_ext["jsonExt"]["niu"]
+                        if hasattr(policy_holder, "json_ext")
+                        and "jsonExt" in policy_holder.json_ext
+                        and "niu" in policy_holder.json_ext["jsonExt"]
+                        else ""
+                    ),
+                    "muncipality": (
+                        policy_holder.locations.parent.name
+                        if policy_holder.locations
+                        else ""
+                    ),
+                    "address": (
+                        policy_holder.address["address"] if policy_holder else ""
+                    ),
+                    "receipt_number": receipt_number if receipt_number else "",
+                    "received_amount": received_amount if received_amount else "",
+                    "tbd": "",  # Add logic for tbd
+                    "contract_number": (
+                        payment.payment_code if payment.payment_code else ""
+                    ),
+                    "phone": (
+                        policy_holder.phone if policy_holder else ""
+                    ),  # Add logic for phone
+                    "receiveddate": formatted_date or "",
+                }
+            }
+            report_config = ReportConfig.get_report(report_name)
+            if not report_config:
+                raise Http404("Report configuration does not exist")
+            report_definition = (
+                report_name, report_config["default_report"]
+            )
+            template_dict = json.loads(report_definition)
+            pdf = generate_report(report_name, template_dict, data)
+            print("Report generated successfully.")
+            return pdf
+    except Exception as e:
+        print(f"An exception occurred: {str(e)}")
+        raise  # Re-raise the exception or handle it according to your requirements
+    print("PDF not generated.")
+    return None  # Handle the case where no PDF is generated
